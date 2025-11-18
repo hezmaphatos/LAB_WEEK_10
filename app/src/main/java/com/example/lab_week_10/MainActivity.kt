4328102934216a23d33fc2d1114a6fdeee45332c
yehezkiel natanael <105317807+hezmaphatos@users.noreply.github.com>
2025-11-18 19:31:07 +0700

    Commit No. 3: Integrate Room Database for Data Persistence

--- app/src/main/java/com/example/lab_week_10/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_10/MainActivity.kt
@@ -8,10 +8,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
+import androidx.room.Room
+import com.example.lab_week_10.database.Total
+import com.example.lab_week_10.database.TotalDatabase
 import com.example.lab_week_10.viewmodels.TotalViewModel
 
 
 class MainActivity : AppCompatActivity() {
+
+    private val db by lazy { prepareDatabase() }
+    // Create an instance of the TotalViewModel
+// by lazy is used to create the ViewModel only when it's needed
     private val viewModel by lazy {
         ViewModelProvider(this)[TotalViewModel::class.java]
     }
@@ -19,8 +26,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Initialize the value of the total from the database
+        initializeValueFromDatabase()
 
         prepareViewModel()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        db.totalDao().update(Total(ID, viewModel.total.value!!))
     }
 
     private fun updateText(total: Int) {
@@ -37,4 +51,34 @@ class MainActivity : AppCompatActivity() {
             viewModel.incrementTotal()
         }
     }
+
+    // Create and build the TotalDatabase with the name 'total-database'
+// allowMainThreadQueries() is used to allow queries to be run on the main thread
+    // This is not recommended, but for simplicity it's used here
+    private fun prepareDatabase(): TotalDatabase {
+        return Room.databaseBuilder(
+            applicationContext,
+            TotalDatabase::class.java, "total-database"
+        ).allowMainThreadQueries().build()
+    }
+
+// Initialize the value of the total from the database
+// If the database is empty, insert a new Total object with the value of 0
+// If the database is not empty, get the value of the total from the database
+    private fun initializeValueFromDatabase() {
+        val total = db.totalDao().getTotal(ID)
+        if (total.isEmpty()) {
+            db.totalDao().insert(Total(id = 1, total = 0))
+        } else {
+            viewModel.setTotal(total.first().total)
+        }
+    }
+
+    // The ID of the Total object in the database
+// For simplicity, we only have one Total object in the database
+// So the ID is always 1
+    companion object {
+        const val ID: Long = 1
+    }
+
 }
